0bd645fa19c5a35a650a06b857a25b4a06043de7
Shasibhusan Jena <shasibhusan.dev@gmail.com>
2025-05-08 23:35:15 +0530

    added

---
+++ AMCEInsuranceProvider/AMCEUserService/src/main/java/AMCEInsurance/AMCEUserService/controller/UserService.java
@@ -0,0 +1,20 @@
+package AMCEInsurance.AMCEUserService.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import AMCEInsurance.AMCEUserService.service.HelloService;
+
+@RestController
+@RequestMapping("/api")
+public class UserService {
+	
+
+	@GetMapping(value = "/hi")
+	public ResponseEntity<String> getHelloMethod(){
+		return new ResponseEntity<>(new HelloService().getHelloMsg(), HttpStatus.OK );
+	}
+}
